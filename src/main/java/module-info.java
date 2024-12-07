8efab652690fb87e69853e01d530a5110bedc30c
SaifKhalifa <khsaif5@gmail.com>
2024-12-07 14:36:16 +0200

    initial project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.groupnine.travelbookingsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.groupnine.travelbookingsystem to javafx.fxml;
+    exports com.groupnine.travelbookingsystem;
+    exports com.groupnine.travelbookingsystem.controller;
+    opens com.groupnine.travelbookingsystem.controller to javafx.fxml;
+}
